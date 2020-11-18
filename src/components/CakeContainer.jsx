edf48f70774f192162a893999706adc143890990
frontendprof <abdumaliksharipov@gmail.com>
2020-11-18 17:15:49 +0500

    Initial setup for redux

---
+++ src/components/CakeContainer.jsx
@@ -0,0 +1,13 @@
+
+import React from 'react'
+
+function CakeContainer() {
+    return (
+        <div>
+            <h2>Number of cakes</h2>
+            <button>Buy Cakes</button>
+        </div>
+    )
+}
+
+export default CakeContainer
